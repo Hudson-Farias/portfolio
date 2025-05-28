e2f3c116471c4e65047c9043b0f876ac59488ecf
Hudson <hudson.farias.dev@gmail.com>
2025-05-28 20:14:27 -0300

    fix: redirect authorship template

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -10,7 +10,7 @@ export function Footer({ socialNetworks }: { socialNetworks: SocialNetworkI[] })
     <div className="container mx-auto my-4 flex flex-col items-center gap-3 text-slate-400 md:flex-row md:justify-between md:gap-0">
       <div className="text-sm flex gap-2 items-center text-center">
         &copy; {year} Template feito por
-        <Link href="https://pinuya.site" target="_blank" className="text-blue-400">Tifany Nunes</Link>
+        <Link href="https://github.com/pinuya" target="_blank" className="text-blue-400">Tifany Nunes</Link>
       </div>
 
       <div className="flex gap-4">
